099d3df6c92627acc7314c6bf0a96d9d76fb684d
Orit Kozolin <orit@nachum.co.il>
2020-12-07 12:54:48 +0200

    UX - put placeholder for artist picture while loading

--- src/features/events/EventItem.js
+++ src/features/events/EventItem.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Typography, Box, Grid, Button } from "@material-ui/core";
 import { useSelector } from "react-redux";
 import { selectEventById } from "./eventsSlice";
-import { Moment, calendarStrings } from "../../utils";
+import { Moment } from "../../utils";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
