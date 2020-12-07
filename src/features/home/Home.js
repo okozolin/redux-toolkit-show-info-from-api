1a6e1ad36edfbb9c982d69f7da54e2163de636cc
Orit Kozolin <orit@nachum.co.il>
2020-12-07 20:06:39 +0200

    Animation - my name fireworks

--- src/features/home/Home.js
+++ src/features/home/Home.js
@@ -16,18 +16,77 @@ const useStyles = makeStyles((theme) => ({
       marginTop: 20,
     },
   },
-  stretchText: {
-    letterSpacing: "normal",
-    transition: theme.transitions.create(["letter-spacing"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    "&:hover": {
-      letterSpacing: "3px",
-      transition: theme.transitions.create(["letter-spacing"], {
-        easing: theme.transitions.easing.easeOut,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
+
+  fireworks: {
+    display: "inline-block",
+    margin: "10px",
+    fontStyle: "normal",
+    position: "relative",
+    "& span": {
+      color: "orange",
+      position: "relative",
+    },
+    "&:hover span": {
+      animation: "$change 1s forwards",
+    },
+    "&::before": {
+      content: ' ""',
+      position: "absolute",
+      top: "calc(50% - 45px)",
+      left: "calc(50% - 45px)",
+      width: "90px",
+      height: "90px",
+      borderRadius: "50%",
+      borderColor: "orange",
+      borderStyle: "solid",
+      borderWidth: "45px",
+      transform: "scale(0)",
+      boxSizing: "border-box",
+    },
+    "&:hover::before": {
+      transition: "transform 0.5s, border-width 0.5s 0.5s",
+      borderWidth: 0,
+      transform: "scale(1)",
+    },
+
+    "&::after": {
+      content: '""',
+      position: "absolute",
+      width: "160px",
+      height: "160px",
+      left: "calc(50% - 80px)",
+      top: "calc(50% - 80px)",
+      background:
+        "radial-gradient(circle, red 50%, transparent 60%), radial-gradient(circle, red 50%, transparent 60%), radial-gradient(circle, red 50%, transparent 60%), radial-gradient(circle, red 50%, transparent 60%), radial-gradient(circle, orange 50%, transparent 60%), radial-gradient(circle, orange 50%, transparent 60%), radial-gradient(circle, orange 50%, transparent 60%), radial-gradient(circle, orange 50%, transparent 60%)",
+      backgroundSize: "16px 16px",
+      backgroundPosition:
+        "calc(50% - 50px) calc(50% - 50px), calc(50% + 50px) calc(50% - 50px), calc(50% - 50px) calc(50% + 50px), calc(50% + 50px) calc(50% + 50px), calc(50% + 0px) calc(50% + 70px), calc(50% + 70px) calc(50% + 0px), calc(50% - 70px) calc(50% + 0px), calc(50% + 0px) calc(50% - 70px)",
+      backgroundRepeat: "no-repeat",
+      borderRadius: "50%",
+      transform: "scale(0)",
+    },
+    "&:hover::after": {
+      transform: "scale(1)",
+      opacity: 0,
+      backgroundSize: "0 0",
+      transition:
+        "transform 0.5s 0.5s, opacity 0.4s 0.9s, background-size 0.5s 0.9s",
+    },
+  },
+
+  "@keyframes change": {
+    "50%": {
+      transform: "scale(0)",
+      filter: "grayscale(100%)",
+    },
+
+    "51%": {
+      filter: "grayscale(0%)",
+    },
+
+    "100%": {
+      transform: "scale(1)",
+      filter: "grayscale(0%)",
     },
   },
 }));
@@ -73,18 +132,21 @@ export default function Home() {
             justify="space-between"
             style={{ padding: "0 20px", marginBottom: "20px" }}
           >
-            <Grid item xs md={4} classes={{ root: classes.stretchText }}>
-              <Box
-                alignItems="center"
-                display="flex"
-                order={{ md: 1, lg: 1 }}
-                color="#ce285d"
-              >
-                <FaceIcon fontSize="large" />
-                <Typography>oritkozolin</Typography>
-              </Box>
+            <Grid item xs md={4}>
+              <div className={classes.fireworks}>
+                <span style={{ display: "inline-block", fontStyle: "normal" }}>
+                  <Box
+                    alignItems="center"
+                    display="flex"
+                    order={{ md: 1, lg: 1 }}
+                    color="#ce285d"
+                  >
+                    <FaceIcon fontSize="large" />
+                    <Typography>oritkozolin</Typography>
+                  </Box>
+                </span>
+              </div>
             </Grid>
-
             <Box clone order={{ md: 2, lg: 3 }}>
               <Grid item xs={6} md>
                 <Navbar />
